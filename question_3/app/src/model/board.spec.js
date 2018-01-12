4089fef584cd87f8b29b12201b6bc2721bc05f36
MAURO ALMEIDA <moalmeida@koinosystems.com>
2018-01-12 00:31:39 -0200

    .

--- question_3/app/src/model/board.spec.js
+++ question_3/app/src/model/board.spec.js
@@ -10,15 +10,19 @@ describe("model/board unit test", () => {
   });
 
   it("should constructor() without error", () => {
-    expect(board.data).toBe();
+    expect(board.data).toBeDefined();
   });
 
   it("should isCellActiveAndCountFewerThanTwo() without error", () => {
     expect(board.isCellActiveAndCountFewerThanTwo(1, 0)).toBe(true);
   });
 
-  it("should isCellActiveAndCountTwoOrThree() without error", () => {
+  it("should isCellActiveAndCountTwoOrThree() to have two neighbours without error", () => {
     expect(board.isCellActiveAndCountTwoOrThree(1, 2)).toBe(true);
+  });
+
+  it("should isCellActiveAndCountTwoOrThree() to have three neighbours without error", () => {
+    expect(board.isCellActiveAndCountTwoOrThree(1, 3)).toBe(true);
   });
 
   it("should isCellActiveAndCountMoreThanThree() without error", () => {
@@ -29,12 +33,21 @@ describe("model/board unit test", () => {
     expect(board.isCellInactiveAndCountExactlyThree(0, 3)).toBe(true);
   });
 
-  it("should setNextGenCell() without error", () => {
-    // expect(board.data).toBe();
+  it("should setNextGenCell() to be active without error", () => {
+    board.setCell(0, 0, 1)
+    board.setCell(0, 1, 1)
+    board.setCell(1, 0, 1)
+    board.setCell(2, 1, 1)
+    board.setNextGenCell(1, 1)
+
+    expect(board.data[1][1]).toBe(1);
   });
 
-  it("should isNextGenCellActive() without error", () => {
-    // expect(board.data).toBe();
+  it("should setNextGenCell() not to be active without error", () => {
+    board.setCell(1, 1, 1)
+    board.setNextGenCell(1, 1)
+
+    expect(board.data[1][1]).toBe(0);
   });
 
 });
